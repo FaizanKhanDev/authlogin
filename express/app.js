bcce6768ae047835434494a9ddbaaa46c7f86db2
FaizanKhanDev <faizan480khan@gmail.com>
2024-03-20 10:52:54 -0700

    FIXED: CORS HAS BEEN FIXED

--- express/app.js
+++ express/app.js
@@ -9,8 +9,22 @@ const app = express()
 const port = process.env.PORT
 const DATABASE_URL = process.env.DATABASE_URL
 
+// allowed origins
+const allowedOrigins = ['http://localhost:3000', ''];
+
+// CORS options
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Check if the origin is in the allowed list
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 // CORS Policy
-app.use(cors())
+app.use(cors(corsOptions))
 
 // Database Connection
 connectDB(DATABASE_URL)
